5f32016eb4aa0dc56be406cd28bbca40970ab3a8
yaKsirhC <chrisuser199@gmail.com>
2023-11-06 20:38:36 +0200

    change cors

--- dist/index.js
+++ dist/index.js
@@ -10,8 +10,11 @@ import { setupListener } from "./utils/databaseListener.utils.js";
 import { connectDatabase } from "./utils/database.utils.js";
 import { handleSocket, handleSocketMessage } from "./utils/socket.utils.js";
 const app = express();
-app.set('port', process.env.PORT || 4001);
-app.use(cors());
+app.set("port", process.env.PORT || 4001);
+app.use(cors({
+    origin: "https://cliq.live",
+    allowCredentials: true
+}));
 app.use(helmet());
 //app.use(limiter)
 app.use(express.json());
@@ -25,17 +28,17 @@ connectDatabase().then(()=>{
     const server = http.createServer(app);
     global.io = new Server(server, {
         cors: {
-            origin: '*'
+            origin: "*"
         }
     });
-    global.io.sockets.on('connection', function(socket) {
+    global.io.sockets.on("connection", function(socket) {
         handleSocket(socket);
-        socket.on('data', function(message) {
+        socket.on("data", function(message) {
             handleSocketMessage(message);
         });
     });
     server.listen(process.env.PORT || 4001);
-    server.on('listening', ()=>console.log('App running'));
+    server.on("listening", ()=>console.log("App running"));
 }).catch(()=>{
-    console.log('Not possible to start server.');
+    console.log("Not possible to start server.");
 });
